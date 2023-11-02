3db91511a4f81f638b9a43256c293ee205f92935
TheSubTop <tsubtnz@gmail.com>
2023-11-02 17:56:48 +1300

    layout stuff

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="text-center font-bold text-orange-300">
+      <h3 className="text-xl pb-4">The Great</h3>
+      <h2 className="text-5xl pb-6">No Nut November 2023</h2>
+      <h1 className="text-8xl uppercase">Edge Battle</h1>
+    </header>
+  );
+}
