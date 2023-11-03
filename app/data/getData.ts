8eeb050f1f6968eef66209c37dd9e0ceef033295
TheSubTop <tsubtnz@gmail.com>
2023-11-04 12:51:38 +1300

    data\!

---
+++ app/data/getData.ts
@@ -0,0 +1,191 @@
+import { https } from "follow-redirects";
+import { createWriteStream, createReadStream } from "fs";
+
+import Papa from "papaparse";
+import { z } from "zod";
+
+const columnNames: { [key: string]: string } = {
+  Timestamp: "timestamp",
+  "Which boy? ": "boy",
+  "Pleasure type": "type",
+  "Mitigating factors": "mitigatingFactors",
+  "Hey there, who's the edger? ": "edger",
+  "How was the session supervised? ": "method",
+  "How long was the supervised edging, in minutes? ": "sessionLength",
+  "How many ruined orgasms did the boy experience? ": "nRuined",
+  "How many full orgasms did the boy experience? ": "nFull",
+  "Anything else important to note about the session? ": "note",
+  "Types of release [Hands free]": "releaseHandsfree",
+  "Types of release [Ruined]": "releaseRuined",
+  "Types of release [Full]": "releaseFull",
+  "Types of release [Ball busting]": "releaseBallBusting",
+  "Types of release [Anal]": "releaseAnal",
+  "Types of release [Nipple]": "releaseNipple",
+  "Types of release [Tapping]": "releaseTapping",
+  "Did everything that came out of the boy go back in to the boy? ": "eatCum",
+  "Did everything that came out of the boy go back in to the boy? _1":
+    "eatCum2",
+};
+
+const Result = z.object({
+  timestamp: z.string(),
+  boy: z.string(),
+  type: z.string(),
+  mitigatingFactors: z.string(),
+  edger: z.string(),
+  method: z.string(),
+  sessionLength: z.string(),
+  nRuined: z.string(),
+  nFull: z.string(),
+  note: z.string(),
+  releaseHandsfree: z.string(),
+  releaseRuined: z.string(),
+  releaseFull: z.string(),
+  releaseBallBusting: z.string(),
+  releaseAnal: z.string(),
+  releaseNipple: z.string(),
+  releaseTapping: z.string(),
+  eatCum: z.string(),
+  eatCum2: z.string(),
+});
+
+type ResponseType = {
+  eatCum: boolean;
+  releases: string[];
+  boy: string;
+  type: string;
+  mitigatingFactors: string;
+  edger: string;
+  method: string;
+  sessionLength: number;
+  note: string;
+  timestamp: Date;
+}[];
+
+function groupBy<K, V>(array: V[], grouper: (item: V) => K) {
+  return array.reduce((store, item) => {
+    var key = grouper(item);
+    if (!store.has(key)) {
+      store.set(key, [item]);
+    } else {
+      store.get(key)!.push(item);
+    }
+    return store;
+  }, new Map<K, V[]>());
+}
+
+export async function getData() {
+  const url = process.env.DATA_URL;
+  if (!url) {
+    return;
+    // return NextResponse.json(
+    //   { error: "Internal server error" },
+    //   { status: 500 }
+    // );
+  }
+
+  await new Promise<void>((resolve) => {
+    https.get(url, (res) => {
+      const fileStream = createWriteStream("data.csv");
+      res.pipe(fileStream);
+
+      fileStream.on("finish", () => {
+        fileStream.close();
+        console.log("Download finished");
+        resolve();
+      });
+    });
+  });
+
+  const file = createReadStream("data.csv");
+
+  //   parse with papaparse
+  const res = await new Promise<ResponseType>((resolve) =>
+    Papa.parse(file, {
+      header: true,
+      complete: (results) => {
+        const data = results.data as { [key: string]: string }[];
+        const d = data.map((d) => {
+          let x = Object.keys(d).map((key) => {
+            const newKey = columnNames[key] || key;
+            return { [newKey]: d[key] };
+          });
+          x = Object.assign({}, ...x);
+          const {
+            timestamp,
+            sessionLength,
+            nRuined,
+            nFull,
+            releaseHandsfree,
+            releaseRuined,
+            releaseFull,
+            releaseBallBusting,
+            releaseAnal,
+            releaseNipple,
+            releaseTapping,
+            eatCum,
+            eatCum2,
+            ...rest
+          } = Result.parse(x);
+          const releases = [
+            { type: "Full", n: parseInt(releaseFull) },
+            { type: "Ruined", n: parseInt(releaseRuined) },
+            { type: "Handsfree", n: parseInt(releaseHandsfree) },
+            { type: "BallBusting", n: parseInt(releaseBallBusting) },
+            { type: "Anal", n: parseInt(releaseAnal) },
+            { type: "Nipple", n: parseInt(releaseNipple) },
+            { type: "Tapping", n: parseInt(releaseTapping) },
+          ]
+            .filter((r) => r.n > 0)
+            .map((r) => Array(r.n).fill(r.type) as string[]);
+          return {
+            timestamp: new Date(timestamp),
+            sessionLength: parseInt(sessionLength),
+            ...rest,
+            eatCum: eatCum === "Yes",
+            releases: releases.flat(),
+          };
+        });
+
+        resolve(d);
+      },
+    })
+  );
+
+  // group into contestants
+  const battle = Array.from(groupBy(res, (x) => x.boy))
+    .map(([key, val]) => ({
+      boy: key,
+      data: val,
+      totalTime: val
+        .map((v) => v.sessionLength)
+        .reduce((total, current) => total + current, 0),
+      penalty: val
+        .map(
+          (v) =>
+            v.releases
+              .map((r) => penaltyMap.get(r) || 0)
+              .reduce((total, current) => total + current, 0) *
+            (v.eatCum ? 1 : 2)
+        )
+        .flat()
+        .reduce((total, current) => total + current, 0),
+    }))
+    .map((x) => ({
+      ...x,
+      score: x.totalTime / x.penalty,
+    }))
+    .sort((a, b) => a.boy.localeCompare(b.boy));
+
+  return battle;
+}
+
+export const penaltyMap = new Map<string, number>([
+  ["Full", 10],
+  ["Ruined", 5],
+  ["Handsfree", 0],
+  ["BallBusting", 4],
+  ["Nipple", 2],
+  ["Anal", -20],
+  ["Tapping", -1],
+]);
--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,92 @@
-"use client";
-
-import { useState } from "react";
+import clsx from "clsx";
 import Header from "./components/Header";
+import { getData } from "./data/getData";
+import { penaltyMap } from "./data/getData";
 
-import Papa from "papaparse";
-
-type Orgasm = {
-  type: string;
-  score: number;
-};
-
-type Score = {
-  score: number;
-  time: number;
-  orgasms: Orgasm[];
-};
-
-type Results = {
-  achilles: Score;
-  tom: Score;
-};
-
-async function getData({ setter }: { setter: Function }) {
-  const url = "";
-
-  Papa.parse(url, {
-    download: true,
-    complete: (results) => {
-      const data = results.data;
-      // const achilles = data.filter((d) => d[1] == "Achilles");
-      // console.log(achilles);
-    },
-  });
-}
+// async function getData() {
+//   const res = await fetch("http://localhost:3000/data");
+//   console.log(res);
+//   if (!res.ok) {
+//     throw new Error("Failed to get data");
+//   }
+//   // console.log(res.json());
+//   // return res.json();
+// }
 
 export default function Home() {
-  const [results, setResults] = useState<Results>();
-
-  getData({ setter: setResults });
-
   return (
     <main className="flex min-h-screen flex-col items-center p-24 text-white bg-gradient-to-b from-black to-orange-950">
       <Header />
-
-      <div className="flex gap-24  mt-12 items-center">
-        <div className="flex-1 text-right text-5xl font-bold">Achilles</div>
-        <div className="text-lg">versus</div>
-        <div className="flex-1 text-5xl font-bold">Tom</div>
+      <div className="grid grid-cols-2 gap-24 mt-12 items-center">
+        <div className="text-right text-4xl font-bold">Achilles</div>
+        <div className="text-4xl font-bold">Tom</div>
       </div>
+
+      <Results />
     </main>
   );
 }
+
+const Results = async () => {
+  const data = await getData();
+  if (!data) return <>Loading ...</>;
+
+  const achilles = data?.filter((x) => x.boy === "Achilles").at(0);
+  const tom = data?.filter((x) => x.boy === "Tom").at(0);
+
+  if (!achilles || !tom) return <>No data ...</>;
+
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="grid grid-cols-2 gap-24 mt-6 items-center justify-evenly text-6xl font-bold text-red-500">
+        <div className="text-right ">
+          {Math.round(achilles.score * 100) / 100}
+        </div>
+        <div className="">{Math.round(tom.score * 100) / 100}</div>
+      </div>
+
+      <div className="grid grid-cols-2 gap-24 items-center justify-evenly text-3xl font-bold">
+        <div className="text-right">{achilles.totalTime} mins</div>
+        <div className="">{tom.totalTime} mins</div>
+      </div>
+
+      <div className="grid grid-cols-2 gap-24 items-center justify-evenly text-3xl font-bold">
+        <div className="text-right flex gap-2 items-center justify-end text-black">
+          {achilles.data
+            .map((d) => d.releases)
+            .flat()
+            .map((r, i) => (
+              <Release key={i} release={r} />
+            ))}
+        </div>
+        <div className="text-right flex gap-2 items-center justify-start text-black">
+          {tom.data.map((d) =>
+            d.releases.map((r, i) => (
+              <Release key={i} release={r} cum={d.eatCum} />
+            ))
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Release = ({ release, cum }: { release: string; cum: boolean }) => {
+  return (
+    <div
+      className={clsx(
+        "h-5 w-5 rounded-full bg-white text-xs flex items-center justify-center",
+        release === "Full" && "bg-red-600 text-white",
+        release === "Ruined" && "bg-orange-600",
+        release === "Handsfree" && "bg-green-600",
+        release === "BallBusting" && "bg-blue-600",
+        release === "Nipple" && "bg-pink-500",
+        release === "Anal" && "bg-gray-600",
+        release === "Tapping" && "bg-lime-600",
+        cum && "border-2"
+      )}
+    >
+      {(penaltyMap.get(release) || 0) * (cum ? 1 : 2)}
+    </div>
+  );
+};
