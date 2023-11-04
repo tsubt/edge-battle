8ce2315e8787472a39d1b6b2ae94e3a72561ead6
TheSubTop <tsubtnz@gmail.com>
2023-11-04 13:09:14 +1300

    stuff

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,12 @@ export default function Header() {
     <header className="text-center font-bold text-orange-300">
       <h3 className="text-xl pb-4">The Great</h3>
       <h2 className="text-5xl pb-6">No Nut November 2023</h2>
-      <h1 className="text-8xl uppercase">Edge Battle</h1>
+      <h1 className="text-8xl uppercase sm:whitespace-nowrap relative z-10">
+        Edge Battle
+        <div className="absolute top-1/2 -translate-x-1/2 text-[12rem] left-1/2 -translate-y-1/2 z-0 opacity-5">
+          Edge Battle
+        </div>
+      </h1>
     </header>
   );
 }
